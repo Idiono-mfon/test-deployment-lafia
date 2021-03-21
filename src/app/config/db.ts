f916cb8c9a8af93372e9ac5cbd644aac59565de1
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-03-21 20:34:55 +0100

    Update db

--- src/app/config/db.ts
+++ src/app/config/db.ts
@@ -1,18 +1,18 @@
-// import knex from 'knex';
-// import { Env } from './env';
-// import { injectable } from 'inversify';
-// import { Model } from 'objection';
-//
-// const env = Env.all().environment;
-// const config = require('../../../knexfile')[env];
-//
-// const db = knex(config);
-//
-// @injectable()
-// export class PostgresConnection {
-//   static getDb() {
-//     return db;
-//   }
-// }
-//
-// Model.knex(db);
+import knex from 'knex';
+import { Model } from 'objection';
+import { Env } from './env';
+
+const env = Env.all().environment;
+const config = require('../../../knexfile')[env];
+
+const db = knex(config);
+
+class PostgresConnection {
+  static getDb() {
+    return db;
+  }
+}
+
+Model.knex(PostgresConnection.getDb());
+
+export { Model, PostgresConnection };
