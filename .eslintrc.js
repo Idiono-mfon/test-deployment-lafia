1d1fc476ec8539a20953085afc019d972b3b5a6c
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-03-21 20:21:33 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,32 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     '@typescript-eslint',
+    'prettier',
   ],
   rules: {
     'import/prefer-default-export': 0,
     'max-classes-per-file': 0,
     'import/extensions': 0,
+    'import/order': 0,
     'import/no-unresolved': 0,
     'no-unused-vars': 0,
     camelcase: 0,
@@ -26,6 +39,8 @@ module.exports = {
     'no-param-reassign': 0,
     'no-console': 0,
     'no-shadow': 0,
-    'comma-dangle': 0
+    'comma-dangle': 0,
+    'lines-between-class-members': 0,
+    'prettier/prettier': 0,
   },
 };
