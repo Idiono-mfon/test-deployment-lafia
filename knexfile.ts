bdd461085012c90c8073ab32e075f4467e402019
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-03-21 20:34:47 +0100

    Update knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,45 +1,70 @@
-// Update with your config settings.
-import { Knex } from 'knex';
-import { Env } from './src/app/config';
+import { Config } from 'knex';
+import { Env } from './src/app/config/env';
 
-const env = Env.all();
+const {
+  environment, pg_dbname,
+  pg_user, pg_password, pg_host,
+  pg_port, pg_test_dbname,
+  pg_test_host, pg_test_password,
+  pg_test_port, pg_test_user,
+} = Env.all();
 
-interface IKnexConfig {
-  development: Knex.Config;
-  staging: Knex.Config;
-  production: Knex.Config;
-  test: Knex.Config;
+interface KnexConfig {
+  [name: string]: Config;
+}
+
+interface Connection {
+  host: string;
+  user: string;
+  password: string;
+  database: string;
+  port: number;
+}
+
+let connection: Connection = {
+  host: pg_host,
+  user: pg_user,
+  password: pg_password,
+  database: pg_dbname,
+  port: pg_port,
+};
+
+if (environment === 'test' || environment === 'testing') {
+  connection = {
+    host: pg_test_host,
+    user: pg_test_user,
+    password: pg_test_password,
+    database: pg_test_dbname,
+    port: pg_test_port,
+  };
 }
 
 const config = {
-  client: 'postgresql',
-  connection: {
-    host: env.pg_host,
-    user: env.pg_user,
-    password: env.pg_password,
-    database: env.pg_dbname
-  },
+  client: 'pg',
+  connection,
   pool: {
     min: 2,
     max: 10
   },
   migrations: {
-    directory: './database/migrations',
+    directory: './src/database/migrations',
     tableName: 'lafia_service_migrations',
+    extension: 'ts',
   },
   seeds: {
-    directory: './database/seeds',
+    directory: './src/database/seeds',
+    extension: 'ts',
   },
 };
 
-const knexConfiguration: IKnexConfig = {
+const knexConfiguration: KnexConfig = {
   development: config,
 
   staging: config,
 
   production: config,
 
-  test: config
+  test: { ...config, debug: true }
 };
 
 module.exports = knexConfiguration;
