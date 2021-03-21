e65599eb1a97f4a0f741eacfc15252342a97ecab
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-03-21 20:36:47 +0100

    Rearrange import order for reflect-metadata to come first

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,9 @@
-import 'reflect-metadata';
 import cors from 'cors';
 import { config as dotConfig } from 'dotenv';
 import express from 'express';
-import { InversifyExpressServer } from 'inversify-express-utils';
-import { Env } from './config';
 import container from './config/inversify.config';
+import { InversifyExpressServer } from 'inversify-express-utils';
+import { Env } from './config/env';
 
 dotConfig();
 
