c405c07aebc07db130f2f6b7160e36b61b5bba9c
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-03-27 06:41:42 +0100

    Remove knex instance initialization from objection

--- src/app/config/db.ts
+++ src/app/config/db.ts
@@ -1,5 +1,4 @@
 import knex from 'knex';
-import { Model } from 'objection';
 import { Env } from './env';
 
 const env = Env.all().environment;
@@ -7,12 +6,8 @@ const config = require('../../../knexfile')[env];
 
 const db = knex(config);
 
-class PostgresConnection {
+export class PostgresConnection {
   static getDb() {
     return db;
   }
 }
-
-Model.knex(PostgresConnection.getDb());
-
-export { Model, PostgresConnection };
