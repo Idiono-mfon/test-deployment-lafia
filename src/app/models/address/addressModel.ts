2eac48d9fdd0b08ceec10b11d208b13bccda2aee
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-03-27 19:27:57 +0100

    Add address model

---
+++ src/app/models/address/addressModel.ts
@@ -0,0 +1,39 @@
+import { Schema, Table } from '../../../database';
+import { BaseModel } from '../base';
+import { IAddress } from './interfaces';
+import { AddressValidator } from './validation';
+
+export class AddressModel extends BaseModel implements IAddress {
+  id!: IAddress['id'];
+  use!: IAddress['use'];
+  city!: IAddress['city'];
+  type!: IAddress['type'];
+  text!: IAddress['text'];
+  line!: IAddress['line'];
+  state!: IAddress['state'];
+  period!: IAddress['period'];
+  country!: IAddress['country'];
+  district!: IAddress['district'];
+  postal_code!: IAddress['postal_code'];
+
+  static get tableName() {
+    return `${Schema.lafiaService}.${Table.address}`;
+  }
+
+  static get jsonSchema() {
+    return AddressValidator;
+  }
+
+  static get relationMappings() {
+    return {
+      period: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: '../periods',
+        join: {
+          from: `${Schema.lafiaService}.${Table.address}.period_id`,
+            to: `${Schema.lafiaService}.${Table.periods}.id`
+        }
+      }
+    }
+  }
+}
---
+++ src/app/models/address/validation/index.ts
@@ -0,0 +1,28 @@
+import { JSONSchema } from 'objection';
+
+export const AddressValidator: JSONSchema = {
+  title: 'Address Validation',
+  type: 'object',
+  required: ['use', 'type'],
+  properties: {
+    id: { type: 'string' },
+    use: { type: 'string' },
+    type: { type: 'string' },
+    text: { type: 'string' },
+    line: { type: 'string' },
+    city: { type: 'string' },
+    district: { type: 'string' },
+    state: { type: 'string' },
+    postal_code: { type: 'string' },
+    country: { type: 'string' },
+    period: {
+      type: 'object',
+      required: ['start'],
+      properties: {
+        id: { type: 'string' },
+        start: { type: 'date-time' },
+        end: { type: 'date-time' }
+      }
+    }
+  }
+};
