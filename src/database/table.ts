1e7c08a5d369b13e14177b502712be5683954360
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-03-29 05:18:58 +0100

    Update table names

--- src/database/table.ts
+++ src/database/table.ts
@@ -1,5 +1,4 @@
 export class Table {
-  public static links = 'links';
   public static users = 'users';
   public static address = 'address';
   public static codings = 'codings';
@@ -11,10 +10,9 @@ export class Table {
   public static identifiers = 'identifiers';
   public static human_names = 'human_names';
   public static organizations = 'organizations';
+  public static patient_links = 'patient_links';
   public static communications = 'communications';
-  public static patients_links = 'patients_links';
   public static contact_points = 'contact_points';
-  public static links_references = 'links_references';
   public static patients_address = 'patients_address';
   public static patient_contacts = 'patient_contacts';
   public static codeable_concepts = 'codeable_concepts';
@@ -23,8 +21,10 @@ export class Table {
   public static patients_identifiers = 'patients_identifiers';
   public static patients_human_names = 'patients_human_names';
   public static identifiers_references = 'identifiers_references';
+  public static patients_patient_links = 'patients_patient_links';
   public static patients_communications = 'patients_communications';
   public static patients_contact_points = 'patients_contact_points';
+  public static patient_links_references = 'patient_links_references';
   public static codeable_concepts_codings = 'codeable_concepts_codings';
   public static patients_patient_contacts = 'patients_patient_contacts';
   public static patient_contacts_contact_points = 'patient_contacts_contact_points';
