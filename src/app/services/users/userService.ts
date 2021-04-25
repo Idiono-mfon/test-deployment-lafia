812e2a1bb3fc72a3f0ff626f0203ebfa993cc224
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-04-25 16:26:50 +0100

    Add User methods for Login, Logout, Find, and update

--- src/app/services/users/userService.ts
+++ src/app/services/users/userService.ts
@@ -1,23 +1,39 @@
 import { inject, injectable } from 'inversify';
 import { v4 as uuid } from 'uuid';
 import TYPES from '../../config/types';
-import { CreateUser } from '../../models';
+import { IUser } from '../../models';
 import { UserRepository } from '../../repository';
-import { Password } from '../../utils/password';
+import { PlatformSdkService } from '../platformSDK';
 
 @injectable()
 export class UserService {
   @inject(TYPES.UserRepository)
   private userRepository: UserRepository;
 
-  public async createUser(user: CreateUser): Promise<any> {
-    const password = await Password.hash(user.password as string);
-    delete user.password;
+  @inject(TYPES.PlatformSdkService)
+  private readonly platformSdkService: PlatformSdkService;
+
+  public async createUser(user: IUser): Promise<IUser> {
     const data = {
       id: uuid(),
       ...user,
-      password,
     };
-    return this.userRepository.createUser(data);
+    return await this.userRepository.createUser(data);
+  }
+
+  public async UserLogin(email: string, password: string): Promise<any> {
+    return await this.userRepository.userLogin(email, password);
+  }
+
+  public async getOneUser(data: IUser): Promise<IUser> {
+    return await this.userRepository.getOneUser(data);
+  }
+
+  public async updateUser(id: string, data: IUser): Promise<IUser> {
+    return await this.userRepository.updateUser(id, data);
+  }
+
+  public async userLogout(id: string): Promise<IUser> {
+    return await this.userRepository.userLogout(id);
   }
 }
