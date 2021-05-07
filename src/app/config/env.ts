a27aeced5dbe6904b427d0a6d0d86d9238dd5471
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-05-07 14:37:59 +0100

    Update twilio environment variables

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -27,6 +27,8 @@ interface IEnv {
   redis_host: string;
   redis_password: string;
   twilio_account_sid: string;
+  twilio_api_key: string;
+  twilio_api_secret: string;
   twilio_auth_token: string;
 }
 
@@ -57,6 +59,8 @@ const config: IEnv = {
   redis_password: process.env.REDIS_PASSWORD as string,
   redis_port: Number(process.env.REDIS_PORT),
   twilio_account_sid: process.env.TWILIO_ACCOUNT_SID as string,
+  twilio_api_key: process.env.TWILIO_API_KEY as string,
+  twilio_api_secret: process.env.TWILIO_API_SECRET as string,
   twilio_auth_token: process.env.TWILIO_AUTH_TOKEN as string,
 };
 
