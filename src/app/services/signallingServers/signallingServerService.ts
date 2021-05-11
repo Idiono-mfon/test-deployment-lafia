b29f10e74143d319fb24720eefa1012d4e0554a3
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-05-11 12:38:36 +0100

    Emit all online practitioners

--- src/app/services/signallingServers/signallingServerService.ts
+++ src/app/services/signallingServers/signallingServerService.ts
@@ -104,7 +104,10 @@ export class SignallingServerService {
       }
     }
 
-    socket.to('onlineUsers').emit('onlinePractitioners', onlinePractitioners);
+    console.log('OnlinePractitioners:', onlinePractitioners);
+    console.log('OnlineUsers:', onlineUsers);
+
+    socket.emit('onlinePractitioners', onlinePractitioners);
   }
 
   private static listenForNewVideoBroadcastEvent(socket: Socket) {
