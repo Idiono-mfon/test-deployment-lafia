a75213e00a4f1fca5257134e89c59b3f9c27403c
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-09-05 14:57:18 +0100

    Add video broadcast details to the practitioners' video log

--- src/app/repository/videoRecords/practitionerVideoBroadcastRepository.ts
+++ src/app/repository/videoRecords/practitionerVideoBroadcastRepository.ts
@@ -35,7 +35,9 @@ export class PractitionerVideoBroadcastRepository {
     
       public async getAllPractitionerBroadcastVideos(user_id: string): Promise<IFindPractitionerVideoBroadcast[]> {
         try {
-          return await PractitionerVideoBroadcastModel.query().where({ practitioner_id: user_id });
+          return await PractitionerVideoBroadcastModel.query()
+            .where({ practitioner_id: user_id })
+            .withGraphFetched(`[videoBroadcast]`);
         } catch (e) {
           throw new InternalServerError(e.message);
         }
