fa303dc2452f4ba56a9c7f1bdf396f63c18fc630
Afolabi Tope <afolabitope@ymail.com>
2021-09-08 15:01:27 +0100

    added delete endpoint

--- src/app/controllers/mediaServer/LafiaVideoController.ts
+++ src/app/controllers/mediaServer/LafiaVideoController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { inject } from 'inversify';
 import {
   controller,
+  httpDelete,
   httpGet,
   request,
   response
@@ -27,6 +28,18 @@ export class LafiaVideoController extends BaseController {
       const broadcast = await this.videoBroadcastService.getAllVideoRecords(user?.id);
 
       this.success(res, broadcast, 'Broadcast fetched successfully', HttpStatusCode.OK);
+    } catch (e) {
+      this.error(res, e);
+    }
+  }
+
+  @httpDelete('/broadcast/:id', TYPES.AuthMiddleware)
+  public async deleteBroadcast(@request() req: Request, @response() res: Response) {
+    try {
+      const { id } = req.params;
+      const broadcast = await this.videoBroadcastService.deleteVideoBroadcastRecords( id );
+
+      this.success(res, broadcast, 'Broadcast delete successfully', HttpStatusCode.OK);
     } catch (e) {
       this.error(res, e);
     }
@@ -59,6 +72,18 @@ export class LafiaVideoController extends BaseController {
 
     } catch (e) {
       console.log(e);
+    }
+  }
+
+  @httpDelete('/broadcast/practitioner/:id', TYPES.AuthMiddleware)
+  public async deletePactitionerBroadcast(@request() req: Request, @response() res: Response) {
+    try {
+      const { id } = req.params;
+      const broadcast = await this.videoBroadcastService.deletePractitionerBroadcastVideo( id );
+
+      this.success(res, broadcast, 'Broadcast delete successfully', HttpStatusCode.OK);
+    } catch (e) {
+      this.error(res, e);
     }
   }
 }
